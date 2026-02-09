53ecbe65a66ece84937455a61d32e92824f2c89e
0xClem <167755388+ClementV78@users.noreply.github.com>
2026-02-09 11:57:49 +0100

    fix(infra): allow node-exporter scrape on 9100 (#356)
    Adds an intra-node security group rule for TCP/9100 so Prometheus can scrape node-exporter on control-plane nodes.

--- infra/aws/modules/k3s/main.tf
+++ infra/aws/modules/k3s/main.tf
@@ -268,6 +268,16 @@ resource "aws_security_group_rule" "k3s_flannel_vxlan" {
   description              = "flannel VXLAN"
 }
 
+resource "aws_security_group_rule" "node_exporter" {
+  type                     = "ingress"
+  security_group_id        = aws_security_group.k3s_nodes.id
+  from_port                = 9100
+  to_port                  = 9100
+  protocol                 = "tcp"
+  source_security_group_id = aws_security_group.k3s_nodes.id
+  description              = "node-exporter metrics"
+}
+
 resource "aws_security_group_rule" "k3s_egress" {
   type              = "egress"
   security_group_id = aws_security_group.k3s_nodes.id
